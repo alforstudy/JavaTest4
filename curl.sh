703d80ebbddee49d73a1f264e5d67cabe8f5332b
安磊 <alforstudy@163.com>
2019-07-26 13:22:06 +0800

    因为个人计算机配置了hosts文件 使用me指向了本机，但是在其它机子上不能使用  顾 修复该问题 使用127.0.0.1

--- curl.sh
+++ curl.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 
-curl -X GET  "me:18080/v1/customers/BySelfPage?page=2&pageSize=10&sort=ASC"
+curl -X GET  "127.0.0.1:18080/v1/customers/BySelfPage?page=2&pageSize=10&sort=ASC"
 echo
-curl -X GET  "me:18080/v1/customers/login?name=AARON&password=SELBY"
+curl -X GET  "127.0.0.1:18080/v1/customers/login?name=AARON&password=SELBY"
 echo
-curl -X GET  "me:18080/v1/customers/?page=2&pageSize=10"
+curl -X GET  "127.0.0.1:18080/v1/customers/?page=2&pageSize=10"
 echo
-curl -X POST -H "Content-Type: application/json" -d '{"firstName":"anlei","lastName":"1997-08-03","email":"alforstudy@163.com","address":"1001 Miyakonojo Lane"}' "me:18080/v1/customers/" > id.json
+curl -X POST -H "Content-Type: application/json" -d '{"firstName":"anlei","lastName":"1997-08-03","email":"alforstudy@163.com","address":"1001 Miyakonojo Lane"}' "127.0.0.1:18080/v1/customers/" > id.json
 echo
 id=$(awk '{print $1}' id.json)
-curl -X PUT -H "Content-Type: application/json" -d '{"firstName":"anlei123","lastName":"nihao","email":"alforstudy@qq.com","address":"1001 Miyakonojo Lane"}' "me:18080/v1/customers/${id}" > id.json
+curl -X PUT -H "Content-Type: application/json" -d '{"firstName":"anlei123","lastName":"nihao","email":"alforstudy@qq.com","address":"1001 Miyakonojo Lane"}' "127.0.0.1:18080/v1/customers/${id}" > id.json
 echo
 id=$(awk -F ','  '{print $1}' id.json | awk -F ":" '{print $2}')
 echo id=$id
-curl -X DELETE  "me:18080/v1/customers/${id}"
+curl -X DELETE  "127.0.0.1:18080/v1/customers/${id}"
 
 
-#curl -X GET me:8080/v1/users/1 > var1
+#curl -X GET 127.0.0.1:8080/v1/users/1 > var1
 
-#curl -X GET me:8080/v1/users/30
+#curl -X GET 127.0.0.1:8080/v1/users/30
 
 
-#curl -X PUT -H "Content-Type: application/json" -d '{"username":"小明123","birthday":"1997-08-06","sex":"2","address":"fd"}' "me:8080/v1/users/31"
-#curl -X DELETE -H "Content-Type: application/json"  "me:8080/v1/users/31"
+#curl -X PUT -H "Content-Type: application/json" -d '{"username":"小明123","birthday":"1997-08-06","sex":"2","address":"fd"}' "127.0.0.1:8080/v1/users/31"
+#curl -X DELETE -H "Content-Type: application/json"  "127.0.0.1:8080/v1/users/31"
 
 
 #curl
